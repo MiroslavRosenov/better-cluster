87b1815c27319099181372f6015491aeb8b3b4c5
chredeur <50155089+chredeur0@users.noreply.github.com>
2022-08-27 19:33:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,6 @@ long_description = (Path(__file__).parent / "README.md").read_text(encoding="UTF
 
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open("requirements.txt") as stream:
-    raw = stream.read().splitlines()
-    requirements = [x for x in raw if not x.startswith("git+")]
-    dependencies = [x for x in raw if x.startswith("git+")]
-
 with open('discord/ext/cluster/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(f.read().decode('UTF-8')).group(1)))
 
@@ -48,8 +43,6 @@ setuptools.setup(
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    install_requires=requirements,
-    dependencies=dependencies,
     keywords=["better-cluster", "cluster", "python", "discord.py"],
     name="better-cluster",
     version=version,
